d7d151d70084d5a4f02f07a0b52344993873188f
JoaoCosme <cosmejoaopedro@protonmail.com>
2021-10-17 12:19:38 -0300

    Criada estrutura de pastas

---
+++ model/DBEntries.java
@@ -0,0 +1,26 @@
+public class DBEntries {
+    private JogadorHashTable jogadores;
+    private UserHashTable users;
+
+    public DBEntries(JogadorHashTable jogadores, UserHashTable users) {
+        this.jogadores = jogadores;
+        this.users = users;
+    }
+
+    public JogadorHashTable getJogadores() {
+        return this.jogadores;
+    }
+
+    public void setJogadores(JogadorHashTable jogadores) {
+        this.jogadores = jogadores;
+    }
+
+    public UserHashTable getUsers() {
+        return this.users;
+    }
+
+    public void setUsers(UserHashTable users) {
+        this.users = users;
+    }
+    
+}
